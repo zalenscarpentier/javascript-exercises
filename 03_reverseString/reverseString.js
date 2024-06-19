4128277173b09b5d6ff8bafad70bd0cce59e9e9c
zalenscarpentier <168520191+zalenscarpentier@users.noreply.github.com>
2024-06-19 18:15:35 +0200

    finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = string.split("").reduce((acc,char) => char + acc,""); 
+    return reverseString;
 };
 
 // Do not edit below this line
