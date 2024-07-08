31fdf1ccb9ab352b106a44bb638db7acf1941da4
zalenscarpentier <168520191+zalenscarpentier@users.noreply.github.com>
2024-07-08 22:53:05 +0200

    finish exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
-
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
+  const findTheOldest = function (people) {
+    return people.sort((a, b) => {
+      const ageA = getAge(a.yearOfBirth, a.yearOfDeath);
+      const ageB = getAge(b.yearOfBirth, b.yearOfDeath);
+      return ageB - ageA; // Sort in descending order of age
+    })[0]; // Return the first element in the sorted array
+  };
+  
 // Do not edit below this line
 module.exports = findTheOldest;
