73de6ef3683b765f29f0cc8db28d9b28976d0910
zalenscarpentier <168520191+zalenscarpentier@users.noreply.github.com>
2024-07-08 21:53:11 +0200

    finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-};
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    return cleanedString === reversedString;
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
